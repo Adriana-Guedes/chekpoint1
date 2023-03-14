a54e88c93cb4490f5b37a7c58e93c7a8c61a17f6
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2023-03-14 12:22:11 -0300

    add, edit e delete ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
 import { useState } from "react";
 
 
-function App (){
+function App() {
 
   //variaveis a serem utilizadas
-  const [id,setId] = useState ("");
-  const [titulo,setTitulo] = useState ("");
-  const [categoria,setCategoria] = useState ("");
-  const [data,setData] = useState ("");
-  const [descricao,setDescricao] = useState ("");
-  const [listaCategoria,setListaCategoria] = useState ([]);//array
+  const [id, setId] = useState("");
+  const [titulo, setTitulo] = useState("");
+  const [categoria, setCategoria] = useState("");
+  const [data, setData] = useState(new Date());
+  const [descricao, setDescricao] = useState("");
+  const [listaCategoria, setListaCategoria] = useState([]);//array
 
 
 
 
   //função para adicionar
-  function adicionar (event){
-    event.preventDefault( )
-    if(categoria === "" || data === "" || descricao === ""){
-      alert("Campos obrigatorio") //ponto extra
-      return 
-    } 
+  function adicionar(event) {
+    event.preventDefault()
+    if (categoria === "" || data === "" || descricao === "") {
+      alert("Campos obrigatórios") //ponto extra
+      return
+    }
     setListaCategoria([...listaCategoria
-    ,{
+      , {
       //criação do objeto
-      id:Date.now(),
+      id: Date.now(),
       data,
       categoria,
       descricao,
@@ -32,111 +32,127 @@ function App (){
 
     }])//spread
     //setar os dados
-        setId("")
-        setData("")
-        setCategoria("")
-        setDescricao("")
-        setTitulo("")
+    setId("")
+    setData("")
+    setCategoria("")
+    setDescricao("")
+    setTitulo("")
 
-      }
-
-
+  }
 
 
-   //função para editar
-   function editar (event){
-    event.preventDefault( )
+
+
+  //função para editar
+  function editar(event) {
+    event.preventDefault()
+
     const copiaListaCategoria = [...listaCategoria]; //spread
-    const index = copiaListaCategoria.findIndex((categoria)=> categoria.id === id);
-    copiaListaCategoria[index].categoria = categoria;
 
+    const index = copiaListaCategoria.findIndex((categoria) => categoria.id === id);
+
+    copiaListaCategoria[index].categoria = categoria;
     copiaListaCategoria[index].data = data;
     copiaListaCategoria[index].titulo = titulo;
     copiaListaCategoria[index].descricao = descricao;
-     
-   }
 
-   
-    //função para apagar
-  function apagar (event){
-    if(confirm("Deseja deletar?")){
-      const result = listaCategoria.filter((item)=> item.id !== id )
+    setListaCategoria(copiaListaCategoria);
+    setId("")
+    setData("")
+    setCategoria("")
+    setDescricao("")
+    setTitulo("")
+
+  }
+
+
+  //função para apagar
+  function apagar(id) {
+    if (confirm("Deseja deletar?")) {
+      const result = listaCategoria.filter((item) => item.id !== id)
       setListaCategoria(result);
     }
-    
-    
+
+
   }
 
 
- //função preencher estados
- function preencher (event){
-  setCategoria(item.categoria);
-  setId(item.id);
-  setData(item.data);
-  setTitulo(item.titulo);
-  setDescricao(item.descricao);
-
-    
- }
+  //função preencher estados
+  function preencher(item) {
+    setId(item.id);
+    setTitulo(item.titulo);
+    setCategoria(item.categoria);
+    setData(item.data);
+    setDescricao(item.descricao);
+  }
 
 
 
-return (
+  return (
 
-<div>
-  <h1> CADASTRAR </h1>
-  <form>
-    <input requered value={titulo} type="text" 
-    placeholder="Titulo" onChange={(event)=> setTitulo(event.target.value) }/>
+    <div className="App">
+      <h1> Filmes para assistir </h1>
 
-   {/*TARGET - para que o valor a ser editado seja localizado  */}
-    <select value={categoria} onChange={(event)=> setCategoria(event.target.value)} >
 
-          <option value="" disabled>Selecione uma categoria</option> 
-          <option value="romance">Romance</option> 
-          <option value="acao">Ação </option> 
-          <option value="comedia">Comedia</option> 
-          <option value="terror">Terror</option> 
-    </select>
-    <input requered value={data} type="date" 
-    placeholder="Data" onChange={(event)=> setData(event.target.value) }/>
+      <form onSubmit={id ? editar : adicionar}>
+        <input
+          required
+          value={titulo}
+          onChange={(event) => setTitulo(event.target.value)}
+          placeholder="Titulo" />
 
-    <input requered value={descricao} type="text" 
-    placeholder="Descricao" onChange={(event)=> setDescricao(event.target.value) }/>
-   <input type="submit" value= {id ? "Salvar":"Cadastrar" }/>
+        {/*TARGET - para que o valor a ser editado seja localizado  */}
+        <select value={categoria} onChange={(event) => setCategoria(event.target.value)} >
 
-  </form>
+          <option value="" disabled>Selecione o gênero</option>
+          <option value="Ação e aventura">Ação e aventura</option>
+          <option value="Comédia">Comedia</option>
+          <option value="Drama">Drama</option>
+          <option value="Ficção científica">Ficção científica</option>
+          <option value="Romance">Romance</option>
+          <option value="Terror">Terror</option>
+        </select>
+        <input requered value={data} type="date"
+          placeholder="Data" onChange={(event) => setData(event.target.value)} />
 
-  {/*retorno de dados salvos*/}
-  {listaCategoria.length > 0 ?
-  (
-    <ul>
-      { listaCategoria.map((item)=> 
-      (
+        <input requered value={descricao} type="textarea"
+          placeholder="Descricao" onChange={(event) => setDescricao(event.target.value)} />
 
-      <li key={item.id} > 
-      <p >{item.titulo} </p> 
-      <p >{item.data} </p> 
-      <p >{item.categoria} </p> 
-      <p >{item.descricao} </p> 
-      <button onClick={()=> apagar(item.id)}>Apagar </button>
-      <button onClick={()=> preencher(item.id)}>Editar </button>
-      </li>
+        <input type="submit" value={id ? "Salvar" : "Cadastrar"} />
 
-      ))}
-    </ul>
-  ):(
-    <p >Nenhuma categoria cadastrada</p> 
+      </form>
+
+      {/*retorno de dados salvos*/}
+      {listaCategoria.length > 0 ?
+        (
+          <ul>
+            {listaCategoria.map((item) =>
+            (
+
+              <li key={item.id} >
+                <p >{item.titulo} </p>
+                <p >{item.data} </p>
+                <p >{item.categoria} </p>
+                <p >{item.descricao} </p>
+
+                <button onClick={() => apagar(item.id)}>Apagar </button>
+                <button onClick={() => preencher(item)}>Editar </button>
+
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p >Nenhuma categoria cadastrada</p>
+        )}
+
+
+    </div>
+
+
+
+
   )
-  }
-
-  
-</div>
-
-
-
-
-)}
+}
 
 
 export default App;
